df12ff17b6ed02b294a4e4c821e0fd9dcf6dc1d3
Armando Berlanga <Jose.berlangam@udem.edu>
2022-02-17 16:08:31 -0600

    diseño inicial

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,39 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+<q-page class="container">
+    <div class="acciones"></div>
+    <div class="procesos"></div>
+    <div class="cpu"></div>
+    <div class="memoria"></div>
+</q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'PageIndex'
-})
+export default {
+    name: 'PageIndex',
+    setup(){
+      document.title = 'Nuestro SO';
+    }
+}
 </script>
+
+<style lang="scss" scoped>
+.container {
+    width: 100%;
+    height: 100vh;
+    display: grid;
+    grid-template-rows: 0.25fr 1fr 1fr 1fr;
+
+    .procesos {
+        background-color: rgb(102, 204, 204);
+    }
+
+    .cpu {
+        background-color: rgb(254, 224, 102);
+    }
+
+    .memoria {
+        background-color: rgb(163, 224, 102);
+    }
+}
+</style>
